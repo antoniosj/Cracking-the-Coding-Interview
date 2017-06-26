b8f7f7084c1d616a72158d32ccb3582cd58fdcea
Antonio Silva Junior <antoniosj@users.noreply.github.com>
2017-06-26 00:00:34 -0300

    Update MyStack.java

--- Data/MyStack.java
+++ Data/MyStack.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package io.github.antoniosj.stack_queue;
 
 import java.util.EmptyStackException;
@@ -41,47 +40,3 @@ public class MyStack<T> {
 		return top == null;
 	}
 }
-=======
-package io.github.antoniosj.stack_queue;
-
-import java.util.EmptyStackException;
-
-public class MyStack<T> {
-
-	private static class StackNode<T> {
-		private T data;
-		private StackNode<T> next;
-
-		public StackNode(T data) {
-			this.data = data;
-		}
-	}
-
-	private StackNode<T> top;
-
-	public T pop() {
-		if (top == null)
-			throw new EmptyStackException();
-
-		T item = top.data;
-		top = top.next;
-		return item;
-	}
-
-	public void push(T item) {
-		StackNode<T> t = new StackNode<T>(item);
-		t.next = top;
-		top = t;
-	}
-
-	public T peek() {
-		if (top == null)
-			throw new EmptyStackException();
-		return top.data;
-	}
-
-	public boolean isEmpty() {
-		return top == null;
-	}
-}
->>>>>>> d9f936943535f12a1a8538cedd22edf952f02691
